5e37b08f15109ead02d934803fa114be0ddabaa2
kapil <kapilmogre1998@gmail.com>
2021-11-29 17:03:39 +0530

    final commit

--- November_coding_Assg/Evaluation/src/component/Dashboard.jsx
+++ November_coding_Assg/Evaluation/src/component/Dashboard.jsx
@@ -1,11 +1,14 @@
-import {movies} from '../db.json';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 
 export const Dashboard = () => {
     return (
         <div>
-          {
+
+            <img src="https://via.placeholder.com/150C/O https://placeholder.com/" alt="img" />
+            <p>
+            <Link to="/movie-details">Watch Recent Movies</Link>
+            </p>
+          {/* {
               movies.map(({id,poster_url,movie_name}) => 
                 <div key={id}>
                     <img src={poster_url} alt="" />
@@ -13,7 +16,7 @@ export const Dashboard = () => {
                     <Link to="/movie-details">Details</Link>
                 </div>
                 )
-          }
+          } */}
         </div>
     )
 }
